3d944ebce2438c9dd28cff6267c7159234bb5331
Lynch071 <jedrzejtomanski@gmail.com>
2020-12-01 11:56:14 +0100

    Update script.js
    adding display number if win

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ document.querySelector('.check').addEventListener('click', function () {
     // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct number!';
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
 
