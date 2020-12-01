2d48f0d97ff56ca881c33f8153eecfa2140020ec
Lynch071 <jedrzejtomanski@gmail.com>
2020-12-01 11:15:15 +0100

    Update script.js
    adding score counter and lost game scenario

--- script.js
+++ script.js
@@ -11,8 +11,22 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct number!';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '🔺 Too high!';
+    if (score > 1) {
+      document.querySelector('.message').textContent = '🔺 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '🔻 Too low!';
+    if (score > 1) {
+      document.querySelector('.message').textContent = '🔺 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
